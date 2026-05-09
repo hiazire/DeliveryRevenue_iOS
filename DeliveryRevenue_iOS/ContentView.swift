fd2db7a398d5989deb9b722c9de91d4939c4965e
RabiSu <hiazire@gmail.com>
2026-05-09 08:29:43 +0800

    Initial Commit

---
+++ DeliveryRevenue_iOS/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  DeliveryRevenue_iOS
+//
+//  Created by 蘇甫瀚 on 2026/5/9.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ DeliveryRevenue_iOS/DeliveryRevenue_iOSApp.swift
@@ -0,0 +1,17 @@
+//
+//  DeliveryRevenue_iOSApp.swift
+//  DeliveryRevenue_iOS
+//
+//  Created by 蘇甫瀚 on 2026/5/9.
+//
+
+import SwiftUI
+
+@main
+struct DeliveryRevenue_iOSApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
